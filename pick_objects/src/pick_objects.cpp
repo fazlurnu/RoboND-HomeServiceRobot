022e87acc3bb63fa0a56604c117b9a27ac14dbfd
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-06-05 09:31:29 +0000

    add param to code

--- pick_objects/src/pick_objects.cpp
+++ pick_objects/src/pick_objects.cpp
@@ -7,9 +7,51 @@
 // Define a client for to send goal requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
+bool readParameters(ros::NodeHandle nodeHandle, float& pick_up_x, float& pick_up_y, float& pick_up_w, float& drop_off_x, float& drop_off_y, float& drop_off_w){
+	if(!nodeHandle.getParam("/pick_objects/pick_up_x", pick_up_x)){
+		return false;
+	}
+	if(!nodeHandle.getParam("/pick_objects/pick_up_y", pick_up_y)){
+		return false;
+	}
+	if(!nodeHandle.getParam("/pick_objects/pick_up_w", pick_up_w)){
+		return false;
+	}
+	if(!nodeHandle.getParam("/pick_objects/drop_off_x", drop_off_x)){
+		return false;
+	}
+	if(!nodeHandle.getParam("/pick_objects/drop_off_y", drop_off_y)){
+		return false;
+	}
+	if(!nodeHandle.getParam("/pick_objects/drop_off_w", drop_off_w)){
+		return false;
+	}
+	return true;
+}
+/*
+float static ;
+float static pick_up_y;
+float static pick_up_w;
+float static drop_off_x;
+float static drop_off_y;
+float static drop_off_w;
+*/
 int main(int argc, char** argv){
   // Initialize the pick_objects node
-  ros::init(argc, argv, "pick_objects");
+  ros::init(argc, argv, "pick_objects");  
+  ros::NodeHandle nodeHandle("~");
+
+  float pick_up_x = 0.0;
+  float pick_up_y = 0.0;
+  float pick_up_w = 0.0;
+  float drop_off_x = 1.0;
+  float drop_off_y = 1.0;
+  float drop_off_w = 1.0;
+
+  if (!readParameters(nodeHandle, pick_up_x, pick_up_y, pick_up_w, drop_off_x, drop_off_y, drop_off_w)){
+		ROS_ERROR("Could not read params.");
+		ros::requestShutdown();
+	}
 
   //tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
@@ -28,7 +70,7 @@ int main(int argc, char** argv){
   // Define a position and orientation for the robot to reach
   int nb_goals = 2;
   int nb_states = 3; // state: x, y, w
-  float goals[nb_goals][nb_states] = { {-2.5, 3.0, 0.0}, {2.5, -3.0, 1.5707}  };
+  float goals[nb_goals][nb_states] = { {pick_up_x, pick_up_y, pick_up_w}, {drop_off_x, drop_off_y, drop_off_w}  };
 
   for (int i=0; i < nb_goals; i++){
 
@@ -37,7 +79,7 @@ int main(int argc, char** argv){
       goal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(goals[i][2]);
 
        // Send the goal position and orientation for the robot to reach
-      ROS_INFO("Sending goal");
+      ROS_INFO("Sending goal [%f], [%f], [%f]", goals[i][0], goals[i][1], goals[i][2]);
       ac.sendGoal(goal);
 
       // Wait an infinite time for the results
