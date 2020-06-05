7db5e687226627467d5aa653a25aef67de7b9fd7
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-06-05 22:20:27 +0000

    fixed error

---
+++ scripts/home_service.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Define workspace variable
+path_catkin_ws="/home/workspace/catkin_ws"
+
+xterm -e "cd ${path_catkin_ws} && source devel/setup.bash && roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 5
+
+xterm -e "cd ${path_catkin_ws} && source devel/setup.bash && roslaunch turtlebot_gazebo amcl_demo.launch" &
+sleep 5
+
+xterm -e "cd ${path_catkin_ws} && source devel/setup.bash && roslaunch add_markers view_navigation.launch" &
+sleep 5
+
+xterm -e "cd ${path_catkin_ws} && source devel/setup.bash && roslaunch add_markers add_markers_with_pickup.launch" &
+sleep 5
+
+xterm -e "cd ${path_catkin_ws} && source devel/setup.bash && roslaunch pick_objects pick_objects.launch" &
