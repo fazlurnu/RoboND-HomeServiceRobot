78844b3c02106847c4af8b6c2eb06a49b758c08e
fazlurnu <fazlur.rahman.ae@gmail.com>
2020-06-05 04:18:24 +0000

    add test_navigation.sh

---
+++ scripts/test_navigation.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+path_catkin_ws="/home/workspace/catkin_ws"
+
+xterm -e "cd ${path_catkin_ws} && source devel/setup.bash && roslaunch turtlebot_gazebo turtlebot_world.launch" &
+
+sleep 5
+
+xterm -e "cd ${path_catkin_ws} && source devel/setup.bash && roslaunch turtlebot_gazebo amcl_demo.launch" &
+
+sleep 5
+
+xterm -e "cd ${path_catkin_ws} && source devel/setup.bash && roslaunch turtlebot_rviz_launchers view_navigation.launch" &
